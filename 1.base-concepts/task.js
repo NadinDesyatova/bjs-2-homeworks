614015acf79617a22e4a6a972e613328da4da3a0
Надя Десятова <nadindinina@yandex.ru>
2023-02-26 19:17:16 +0300

    Реализовано первое задание по теме Основные понятия

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,24 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  
+  if (discriminant == 0) {
+    let squareRoot = - b / (2 * a);
+    arr.push(squareRoot); 
+  } else if (discriminant > 0) {
+      let squareRoot1 = (- b + Math.sqrt(discriminant)) / (2 * a);
+      let squareRoot2 = (- b - Math.sqrt(discriminant)) / (2 * a);
+      arr.push(squareRoot1);
+      arr.push(squareRoot2);
+    } else if (discriminant < 0) {
+      return arr;
+      }
   
   return arr;
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
