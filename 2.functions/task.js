5d9df24e39b5130edc78c6948aa771e18de3d21f
Надя Десятова <nadindinina@yandex.ru>
2023-03-01 14:07:37 +0300

    Первое задание

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,10 @@
 function getArrayParams(...arr) {
-
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  min = Math.min(arr);
+  max = Math.max(arr);
+  
   return { min: min, max: max, avg: avg };
 }
 
