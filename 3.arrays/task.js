75c573ffe6fbb7ec00f288956c3cafea70e286ee
Надя Десятова <nadindinina@yandex.ru>
2023-03-06 20:37:44 +0300

    Выполнено задание на тему Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index])) {
+    return true;
+  } else {
+  	  return false;
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+  let arr = [...users];
+  let result = arr.filter(user => user.gender === gender).reduce((accum, user, index, arr) => {
+      accum += user.age;
+      if (index === arr.length - 1) {
+        return accum / arr.length;
+      }
+      return accum;
+  }, 0);
   
+  return result;
 }
