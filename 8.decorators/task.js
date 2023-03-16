305021266bf959f034db64600659970f083202e9
Надя Десятова <nadindinina@yandex.ru>
2023-03-16 13:31:29 +0300

    Предварительное решение задания по теме «Декораторы»

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,52 @@
-//Задача № 1
+// Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = md5(args);
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value;
+    }
+
+    let result = func(...args);
+    cache.push({hash, value: result});
+    if (cache.length > 5) { 
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+  }
+  return wrapper; 
 }
 
-//Задача № 2
+// Задача № 2
 function debounceDecoratorNew(func, delay) {
+  let isThrottled = false;
+  let timeoutId = null;
+  function wrapper(...args) { 
+    wrapper.allCount++;  
+    if (timeoutId === null) {
+      wrapper.count++;
+      timeoutId = 0;
+      return func(...args);
+    } 
+    isThrottled = true;
+
+    timeoutId = setTimeout(() => {
+      isThrottled = false;
+      wrapper.count++;
+      return func(...args);
+    }, delay);
+
+    if (isThrottled) {
+      return;
+    }
+  }
+
+  wrapper.allCount = 0;
+  wrapper.count = 0;
   
+  return wrapper;
 }
